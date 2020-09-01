6576614fe73a7e688548e11deea85e7f5efdc344
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-01 17:14:03 +0000

    aditional doble quote in comment at line 2 removed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""-*- coding: utf-8 -*-""""
+"""-*- coding: utf-8 -*-"""
 from flask import Flask
 
 app = Flask(__name__)
+ss = {'strict_slashes': False}
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/', **ss)
 def index():
     """Index"""
     return ('Hello HBNB!')
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb', **ss)
 def hbnb():
     """hbnb"""
     return 'HBNB'
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(port=5000)
