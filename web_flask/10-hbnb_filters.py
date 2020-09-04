63d11e9edcef72241045571c2d10e9f59bfd47fd
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-04 22:44:56 +0000

    web_flask/10-hbnb_filters.py added

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""-*- coding: utf-8 -*-"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+ss = {'strict_slashes': False}
+
+
+@app.teardown_appcontext
+def teardown_db(e):
+    """closes the storage"""
+    storage.close()
+
+
+@app.route('/hbnb_filters', **ss)
+def hbnb_filters():
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    amenities = sorted(list(storage.all(Amenity).values()),
+                       key=lambda x: x.name)
+    print(type(amenities))
+    return render_template(
+        '10-hbnb_filters.html', states=states, amenities=amenities)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
