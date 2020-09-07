c43e2f2449ad5a622129170a2fab2e1bd8995304
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-07 21:09:44 +0000

    Testing PUSHER - models/review.py

--- models/review.py
+++ models/review.py
@@ -15,9 +15,9 @@ class Review(BaseModel, Base):
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'))
         user_id = Column(String(60),
                          ForeignKey('users.id'), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'))
     else:
         place_id = ""
         user_id = ""
